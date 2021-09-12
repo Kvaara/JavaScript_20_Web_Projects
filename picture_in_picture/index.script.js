99c41093147dd6f6b972c8616956aa45c3ceb5ad
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-09-12 22:00:26 +0300

    This Picture-in-Picture project is now done. I improved the previous version a bit. It now only prompts the user to share a screen IF the user clicks the START button. In addition the screen recording ends if the user closes the PiP window and in contrast clicking the 'Stop sharing' button at the bottom now correctly closes the PiP window (it doesn't hang empty on the page anymore).

--- picture_in_picture/index.script.js
+++ picture_in_picture/index.script.js
@@ -2,27 +2,37 @@
 const videoElement = document.querySelector("#video");
 const startBtn = document.querySelector("#start-btn");
 
-const mediaStreamSelector = async () => {
+const mediaStreamSelector = async (callback) => {
   try {
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     videoElement.srcObject = mediaStream;
-    videoElement.onloadedmetadata = () => videoElement.play;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+
+      // Eventlistener that closes the PiP window after user clicks the 'Stop sharing' button
+      mediaStream.getVideoTracks()[0].onended = () => {
+        document.exitPictureInPicture();
+      };
+
+      // Eventlistener for when the user clicks the close button (x) in the PiP window
+      videoElement.addEventListener("leavepictureinpicture", () => {
+        videoElement.srcObject.getTracks()[0].stop();
+      });
+
+      return callback(); // Return the callback and run the anonymous function
+    };
   } catch (error) {
     console.log("An unexpected error occurred:", error);
   }
 };
 
 // Eventlistener for the START button
-startBtn.addEventListener("click", async () => {
+startBtn.addEventListener("click", () => {
   startBtn.disabled = true;
 
-  // Start picture-in-picture
-  await videoElement.requestPictureInPicture();
-
-  startBtn.disabled = false;
-});
-
-// Call the function after the whole DOM has been loaded
-window.addEventListener("load", () => {
-  mediaStreamSelector();
+  mediaStreamSelector(async () => {
+    // Start picture-in-picture
+    await videoElement.requestPictureInPicture();
+    startBtn.disabled = false;
+  });
 });

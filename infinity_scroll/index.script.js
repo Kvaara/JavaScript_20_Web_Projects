b1d3252342d217a9f1d8b65b0cb79de84b5ca524
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-09-12 05:14:20 +0300

    Infinite/endless scrolling is now implemented. This project is now done. There is some improvements that can be done to the code like changing a couple of the functions to be pure functions. In addition, we could scale down the images so that the page would load fast for people with slow internet connection.

--- infinity_scroll/index.script.js
+++ infinity_scroll/index.script.js
@@ -1,12 +1,26 @@
 // Declaring the DOM elements needed to const variables
 const imgContainer = document.querySelector("#img-container");
-const loader = document.querySelector("#loading-spinner");
+const loadingSpinner = document.querySelector("#loading-spinner");
+
+// Variables for the infinite/endless scroll functionality
+let allImagesAreLoaded = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+
+// Function that runs after an img in the DOM is loaded
+const imageHasLoaded = () => {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    allImagesAreLoaded = true;
+    loadingSpinner.hidden = true;
+  }
+};
 
 // Function that's used to fetch a photoCount number of photos from Unsplash
-const getRandomImagesArray = async (photoCount = 30) => {
+const getRandomImagesArray = async (imageCount) => {
   // Declaring the const variables needed to fetch images using Unsplash's API
   const accessKey = "foMCRiZsqZSwpCgQ0otHwbQ3A6-vF3tLCpdaclpfRgQ";
-  const UnsplashAPI = `https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${photoCount}`;
+  const UnsplashAPI = `https://api.unsplash.com/photos/random/?client_id=${accessKey}&count=${imageCount}`;
 
   try {
     const result = await fetch(UnsplashAPI, {
@@ -21,9 +35,12 @@ const getRandomImagesArray = async (photoCount = 30) => {
 };
 
 // Generate the image elements to the DOM using the getRandomPhotosArray function
-const photosToImgContainer = async () => {
-  const imagesArray = await getRandomImagesArray();
-  console.log(imagesArray);
+// In addition, implement the infinite/endless scroller
+const photosToImgContainer = async (imageCount) => {
+  const imagesArray = await getRandomImagesArray(imageCount);
+  totalImages += imagesArray.length;
+
+  // Construct the images into the DOM
   imagesArray.forEach((image) => {
     // Creating an 'a' tag to be the container of the image
     const item = document.createElement("a");
@@ -34,6 +51,8 @@ const photosToImgContainer = async () => {
     img.src = image.urls.regular;
     img.alt = image.alt_description;
     img.title = image.alt_description;
+    // Eventlistener that triggers after the corresponding img is loaded
+    img.addEventListener("load", imageHasLoaded);
     // Appending the 'img' child into the 'a' container
     item.appendChild(img);
     // Appending the 'a' container with the img into the img-container element
@@ -41,4 +60,18 @@ const photosToImgContainer = async () => {
   });
 };
 
-photosToImgContainer();
+// The infinite/endless scroll functionality itself
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    allImagesAreLoaded
+  ) {
+    allImagesAreLoaded = false;
+    photosToImgContainer(5);
+  }
+});
+
+// Eventlistener that triggers after the whole DOM has been loaded
+window.addEventListener("load", () => {
+  photosToImgContainer(10);
+});

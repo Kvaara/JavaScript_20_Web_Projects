77da26e6d81fa1b2bf21b51a48dc1811f6db8d8e
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-09-19 17:33:26 +0300

    Joke API is now implemented. Now you can press the 'Tell me a joke' button and the program will then use the text-to-speech API to tell you a joke, which has been fetched by using the joke API.

--- joke_teller/index.script.js
+++ joke_teller/index.script.js
@@ -2,10 +2,11 @@
 const newJokeButton = document.querySelector("#joke-btn");
 const audioElement = document.querySelector("#joke-audio");
 
-const test = () => {
+// Helper function that builds the text-to-speech using VoiceRSS SDK by using the joke data
+const buildTextToSpeech = ({ setup, delivery }) => {
   VoiceRSS.speech({
     key: "2d6b81808c2d4fa389a545bc2fd84f4a",
-    src: "Hello, world!",
+    src: `${setup} ... ${delivery}`,
     hl: "en-us",
     r: 0,
     c: "mp3",
@@ -13,4 +14,24 @@ const test = () => {
     ssml: false,
   });
 };
-test();
+
+// Helper function that fetches a response from the jokeapi API
+const fetchAJoke = async () => {
+  try {
+    const response = await fetch(
+      "https://v2.jokeapi.dev/joke/Programming?type=twopart"
+    );
+    const data = await response.json();
+    const { setup, delivery } = data;
+    return { setup, delivery };
+  } catch (err) {
+    alert("An error occurred. Sorry about that...");
+    console.log(err);
+  }
+};
+
+// Click event listener for the "Tell me a joke" button
+newJokeButton.addEventListener("click", async () => {
+  const jokeData = await fetchAJoke();
+  buildTextToSpeech(jokeData);
+});
